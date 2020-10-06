28903e519e80b550f03d31783d5131767879d7c7
Angelo Correia <angelocorreia27@hotmail.com>
2020-10-06 20:41:18 -0100

    add firebase

--- src/App.js
+++ src/App.js
@@ -3,6 +3,27 @@ import React from 'react';
 import Routes from "./routes";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+// Firebase App (the core Firebase SDK) is always required and
+// must be listed before other Firebase SDKs
+import * as firebase from "firebase/app";
+
+// Add the Firebase services that you want to use
+import "firebase/auth";
+import "firebase/firestore";
+import "firebase/database";
+import * as env from './env';
+
+ var firebaseConfig = {
+  apiKey: env.REACT_APP_APIKEY,
+  authDomain: env.REACT_APP_AUTHDOMAIN,
+  databaseURL: env.REACT_APP_DATABASEURL,
+  projectId: env.REACT_APP_PROJECTID,
+  storageBucket: env.REACT_APP_STORAGEBUCKET,
+  messagingSenderId: env.REACT_APP_MESSAGINGSENDERID,
+  appID: env.REACT_APP_APPID,
+};
+firebase.initializeApp(firebaseConfig);
+
 function App() {
   return (
     
--- src/component/AtividadesDesenvolver/index.js
+++ src/component/AtividadesDesenvolver/index.js
@@ -1,6 +1,13 @@
 import React from 'react';
-import { Form } from 'react-bootstrap'
+import { Form, Col, Row } from 'react-bootstrap'
+// Firebase App (the core Firebase SDK) is always required and
+// must be listed before other Firebase SDKs
+import * as firebase from "firebase/app";
 
+// Add the Firebase services that you want to use
+import "firebase/auth";
+import "firebase/firestore";
+import "firebase/database";
 const optionList = ['Música', 'Teatro', 'Karaoke', 'Dança', 'Tauromaquia', 'Circo'];
 
 class AtividadesDesenvolver extends React.Component {
@@ -9,10 +16,50 @@ class AtividadesDesenvolver extends React.Component {
         this.state = {
             checkbox: null,
             act_art_outros: null,
-            actividades_artisticas:null,
-            act_cin_outros:null,
-            act_vid_outros:null,
+            actividades_artisticas: null,
+            act_cin_outros: null,
+            act_vid_outros: null,
         }
+    }
+    submitHendler(){
+        console('acção gravar');
+    }
+    componentDidMount() {
+    
+        // tenta carrega informações de um no (players)
+        var ref = firebase.database().ref('aluno');
+
+        console.log(ref);
+
+        // Gravação de dados
+        var playersRef = firebase.database().ref("aluno/");
+
+        playersRef.set({
+            Igor: {
+                number: 1,
+                age: 30
+            },
+
+            Monica: {
+                number: 2,
+                age: 20
+            }
+        });
+
+        // Update
+        var johnRef = firebase.database().ref("aluno/Monica");
+        johnRef.update({
+            "age": 40
+        });
+
+        // Query
+        var playersRefFiltro = firebase.database().ref("aluno/");
+
+        playersRefFiltro.orderByChild("age").on("child_added", function (data) {
+            console.log(data.val().age);
+        });
+
+
     }
     changeHandler(event) {
         event.preventDefault();
@@ -38,13 +85,12 @@ class AtividadesDesenvolver extends React.Component {
                    </p> */}
 
                         {optionList.map((type) => (
-                            <div key={`inline-${type}`} className="mb-3">
+                            <Col sm={10}>
                                 <Form.Label>{type}</Form.Label>
-                                <Form.Check name="actividades_artisticas" inline id={`inline-${type}-1`} 
-                                value={type}
-                                onChange={(event) => { this.changeHandler(event) }}/>
-
-                            </div>
+                                <Form.Check name="actividades_artisticas" inline id={`inline-${type}-1`}
+                                    value={type}
+                                    onChange={(event) => { this.changeHandler(event) }} />
+                            </Col>
                         ))}
                     </Form.Group>
                     <Form.Group controlId="exampleForm.ControlTextarea1">
@@ -59,7 +105,7 @@ class AtividadesDesenvolver extends React.Component {
                     </p>
                     <Form.Group controlId="exampleForm.ControlTextarea1">
                         <Form.Label>Outras</Form.Label>
-                        <Form.Control as="textarea" rows={3} name="act_cin_outros" onChange={(event) => { this.changeHandler(event) }}/>
+                        <Form.Control as="textarea" rows={3} name="act_cin_outros" onChange={(event) => { this.changeHandler(event) }} />
                         <p>150 caracteres para atingir o máximo permitido</p>
                     </Form.Group>
                     <h1>Atividades Videográficos</h1>
@@ -71,7 +117,7 @@ class AtividadesDesenvolver extends React.Component {
                     </p>
                     <Form.Group controlId="exampleForm.ControlTextarea1">
                         <Form.Label>Outras</Form.Label>
-                        <Form.Control as="textarea" rows={3} name="act_vid_outros" onChange={(event) => { this.changeHandler(event) }}/>
+                        <Form.Control as="textarea" rows={3} name="act_vid_outros" onChange={(event) => { this.changeHandler(event) }} />
                         <p>150 caracteres para atingir o máximo permitido</p>
                     </Form.Group>
                 </Form>
