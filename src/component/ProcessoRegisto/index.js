d1eab67c4c27edb8681b2ed747c8389f1a46f86d
MF / UTIC - Hermes Mauricio <Hermes.Mauricio@gov.cv>
2020-10-08 16:44:32 -0100

    ProcessoRegisto

---
+++ src/component/ProcessoRegisto/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class ProcessoRegisto extends React.Component {
+    render(){
+        return (
+            <div>
+                <p> A desenvolver ProcessoRegisto</p>
+            </div>
+        );
+    }
+}
+
+export default ProcessoRegisto;
