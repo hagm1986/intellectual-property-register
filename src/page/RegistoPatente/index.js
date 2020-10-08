80ef9f9e0138c94f4cd8b5266f208c3c4678a605
MF / UTIC - Hermes Mauricio <Hermes.Mauricio@gov.cv>
2020-10-08 21:23:59 -0100

    increment

--- src/page/RegistoPatente/index.js
+++ src/page/RegistoPatente/index.js
@@ -27,26 +27,59 @@ class RegistoPatente extends React.Component {
     constructor(props){
         super(props);
         this.state = {
-            dadosRequerente:null
+            dadosRequerente:null,
+            dadosDesenvolver:null,
+            dadosRegisto:null,
+            dadosReivindicacao:null,
+            dadosViewfigura:null
         }
+
         this.callBackReq = this.callBackReq.bind(this);
+        this.callBackDev = this.callBackDev.bind(this);
+        this.callBackPreg = this.callBackPreg.bind(this);
+        this.callBackRei = this.callBackRei.bind(this);
+        this.callBackVfig = this.callBackVfig.bind(this);
+
         this.submmitHandler = this.submmitHandler.bind(this);
     }
    
     callBackReq(dadosReq){
         this.setState({dadosRequerente:dadosReq});
     }
+    callBackDev(dadosDev){
+        this.setState({dadosDesenvolver:dadosDev});
+    }
+    callBackPreg(dadosPreg){
+        this.setState({dadosRegisto:dadosPreg});
+    }
+    callBackRei(dadosRei){
+        this.setState({dadosReivindicacao:dadosRei});
+    }
+    callBackVfig(dadosVfig){
+        this.setState({dadosViewfigura:dadosVfig});
+    }
     submmitHandler(){
 
         console.log('Acção Gravar!');
         console.log('state: ', this.state.dadosRequerente);
+        console.log('state: ', this.state.dadosDesenvolver);
+        console.log('state: ', this.state.dadosRegisto);
+        console.log('state: ', this.state.dadosReivindicacao);
+        console.log('state: ', this.state.dadosViewfigura);
 
          // Tenta carrega informações de um no (players)
          var reqRef = firebase.database().ref('dadosRequerente');
-         
+         var reqDev = firebase.database().ref('dadosDesenvolver');
+         var reqReg = firebase.database().ref('dadosRegisto');
+         var reqRei = firebase.database().ref('dadosReivindicacao');
+         var reqView= firebase.database().ref('dadosViewfigura');
+
          // Gravação de dados
          reqRef.push(this.state.dadosRequerente);
-
+        reqDev.push(this.state.dadosDesenvolver);
+         reqReg.push(this.state.dadosRegisto);
+         reqRei.push(this.state.dadosReivindicacao);
+         reqView.push(this.state.dadosViewfigura);
 
     }
     render() {
@@ -63,31 +96,30 @@ class RegistoPatente extends React.Component {
                 <Card>
                     <Card.Header>Actividades a Desenvolver </Card.Header>
                     <Card.Body>
-                        <AtividadesDesenvolver parentReq={this.callBackReq.bind(this)}/>
+                        <AtividadesDesenvolver parentDev={this.callBackDev.bind(this)}/>
                     </Card.Body>
                 </Card>
 
                 <Card>
                     <Card.Header>Processo de Registo </Card.Header>
                     <Card.Body>
-                        <ProcessoRegisto parentReq={this.callBackReq.bind(this)}/>
+                        <ProcessoRegisto parentReq={this.callBackPreg.bind(this)}/>
                     </Card.Body>
                 </Card>
 
                 <Card>
                     <Card.Header>Reinvidicacao </Card.Header>
                     <Card.Body>
-                        <Reinvidicacao parentReq={this.callBackReq.bind(this)}/>
+                        <Reinvidicacao parentReq={this.callBackRei.bind(this)}/>
                     </Card.Body>
                 </Card>
 
                 <Card>
                     <Card.Header>View Figura </Card.Header>
                     <Card.Body>
-                        <ViewFigura parentReq={this.callBackReq.bind(this)}/>
+                        <ViewFigura parentReq={this.callBackVfig.bind(this)}/>
                     </Card.Body>
                 </Card>
-
                 <br/>
                 <Button className="pull-right" 
                         variant="success"
